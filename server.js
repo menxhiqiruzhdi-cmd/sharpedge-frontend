6a6ea378dd9eec08182f5a2e60f309f0add6068c
menxhiqiruzhdi-cmd <menxhiqiruzhdi@gmail.com>
2026-04-27 05:55:52 -0400

    server.js

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+// Serve static files from root directory
+app.use(express.static(__dirname));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
