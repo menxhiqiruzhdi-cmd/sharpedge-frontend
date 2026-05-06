32faf26c6315a2c6253385e2dc7926bedb26ee57
menxhiqiruzhdi-cmd <menxhiqiruzhdi@gmail.com>
2026-05-06 01:18:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Serve static files from root directory
 app.use(express.static(__dirname));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`SharpEdge frontend running on port ${PORT}`);
-});
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "landing.html")));
+app.get("/app", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.listen(PORT, () => console.log(`SharpEdge running on port ${PORT}`));
