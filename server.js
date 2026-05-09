c0f5b020fe71ad1fb5e3b5365f86e99dfdc11987
menxhiqiruzhdi-cmd <menxhiqiruzhdi@gmail.com>
2026-05-08 22:27:35 -0400

    server.js

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.use(express.static(__dirname));
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "landing.html")));
-app.get("/app", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
-app.listen(PORT, () => console.log(`SharpEdge running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "landing.html"));
+});
+
+app.get("/app", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "landing.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`SharpEdge running on port ${PORT}`);
+  console.log(`Files in directory:`, fs.readdirSync(__dirname).join(", "));
+});
