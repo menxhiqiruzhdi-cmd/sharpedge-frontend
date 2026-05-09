7cfd30f45343d229d5ac30b41034eee325f93c9b
menxhiqiruzhdi-cmd <menxhiqiruzhdi@gmail.com>
2026-05-08 23:02:07 -0400

    server.js

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
-// Root serves the landing page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "landing.html"));
 });
 
-// Platform app
 app.get("/app", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "landing.html"));
+});
+
 app.listen(PORT, () => {
-  console.log(`SharpEdge frontend running on port ${PORT}`);
+  console.log(`SharpEdge running on port ${PORT}`);
+  console.log(`Files:`, fs.readdirSync(__dirname).filter(f => f.endsWith('.html') || f.endsWith('.js')).join(", "));
 });
